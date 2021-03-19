8357ecb813399b671be3cea5850ce626a976eaa3
Belmiro Mungoi <56709665+BelmiroMungoi@users.noreply.github.com>
2021-03-19 15:48:53 +0200

    Converte a temperatura de graus celsius para faranheit

---
+++ Exercicios/ConversorTemp.java
@@ -0,0 +1,14 @@
+// Declaracao da classe;
+public class ConversorTemp{
+
+    // Metodo principal;
+    public static void main(String[] args){
+
+        // Operacoes matematicas;
+        float grausc = (float) 32.5;
+        float grausf = ((9 * grausc) / 5) + 32;
+
+        // Impressao dos resultados;
+        System.out.println("A temperatura de "+grausc+" graus celsos, corresponde a "+grausf+" graus faranheits") ;
+    }
+}
