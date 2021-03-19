d2b82c1def40e3de119fe0e673235524f69608e9
Belmiro Mungoi <56709665+BelmiroMungoi@users.noreply.github.com>
2021-03-19 15:43:21 +0200

    Calcula a area de uma circuferencia

---
+++ Exercicios/AreaCir.java
@@ -0,0 +1,14 @@
+// Declaracao da classe principal;
+public class AreaCir{
+
+    // Metodo principal;
+    public static void main(String[] args ) {
+
+        // Operacoes matematicas;
+        byte r = 4 ; float a = (float) (Math.PI * 4) ;
+        int d = (int) r * 2;
+
+        // Impressao dos resultados;
+        System.out.println("A area da circuferencia com raio "+r+" e: "+a+" e o seu dobro e: "+d)   ;
+    }
+}
