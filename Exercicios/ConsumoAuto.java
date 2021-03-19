40b3653b28d2490a7d52ef73a8c214dc6087d1f3
Belmiro Mungoi <56709665+BelmiroMungoi@users.noreply.github.com>
2021-03-19 15:48:12 +0200

    Calcula o consumo de gasolina de um automovel

---
+++ Exercicios/ConsumoAuto.java
@@ -0,0 +1,15 @@
+// Declaracao da classe principal;
+public class ConsumoAuto{
+
+    // Metodo principal;
+    public static void main(String[] args){
+
+        // Declaracao da variavel e operacao matematica;
+        float d = (float) 29.5329;
+        float c = (float) 14.5;
+        float cm = (d/c) ;
+
+        //Impressao do resultado;
+        System.out.println("O consumo medio do automovel e "+cm);
+    }
+}
