ba542817085158df01b9e58f71fb33d22d6b5eed
Belmiro Mungoi <56709665+BelmiroMungoi@users.noreply.github.com>
2021-03-19 15:47:31 +0200

    Produto com while

---
+++ Exercicios/Calcula.java
@@ -0,0 +1,10 @@
+public class Calcula{
+    public static void main(String[]args){
+        int a =1, b = 1;
+        while(b<=5){
+             int prod = a*b;
+             System.out.println(a+"*"+b+"="+prod);
+             b = b+1;
+        }
+    }
+}
