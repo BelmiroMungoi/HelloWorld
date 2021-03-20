ce4acbbe3ddc2c492d19617c446c4685b5c93125
Belmiro Mungoi <56709665+BelmiroMungoi@users.noreply.github.com>
2021-03-20 19:41:21 +0200

    Verifica qual e o maior numero dentro do ciclo

---
+++ Exercicios/MaiorNumero2.java
@@ -0,0 +1,20 @@
+//Exercicio numero 7.
+import javax.swing.JOptionPane;
+
+public class MaiorNumero2 {
+
+    public static void main(String[] args){
+    int maior;
+    int n = Integer.parseInt(JOptionPane.showInputDialog("Insira um numero: "));
+    maior = n ;
+
+
+          for(int i = 2; i<=15; i++){
+              int num = Integer.parseInt(JOptionPane.showInputDialog("Insira um numero: "));
+              if (num<num);
+              maior = num;
+
+          }
+          JOptionPane.showMessageDialog(null,"O maior numero e "+maior);
+    }
+}
