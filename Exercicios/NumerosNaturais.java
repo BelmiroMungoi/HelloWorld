aa851e2e79783808fe2dc7accacc9162e001a8e7
Belmiro Mungoi <56709665+BelmiroMungoi@users.noreply.github.com>
2021-03-20 17:50:51 +0200

    Imprime os 100 primeiros numeros

---
+++ Exercicios/NumerosNaturais.java
@@ -0,0 +1,11 @@
+//Exercicio numero 1.
+public class NumerosNaturais {
+    public static void main(String[] args) {
+
+
+        for (int num=1; num<=100; num++ ) {
+            System.out.println(num);
+
+        }
+    }
+}
